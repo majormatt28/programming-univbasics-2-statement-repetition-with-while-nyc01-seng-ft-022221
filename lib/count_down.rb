4a316aaac91b3d1d309a8ad1a9f9f98790129be0
Mathew E Newton <elijah2895@hotmail.com>
2021-01-31 17:16:58 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,19 @@
 # Write your code here
+count = 0
+magic_exit_number = 10
+
+while count < 12 do
+  break if count == magic_exit_number
+  puts 10
+  puts 9
+  puts 8
+  puts 7
+  puts 6
+  puts 5
+  puts 4
+  puts 3
+  puts 2
+  puts 1
+  puts "Happy New Year!"
+  count += 1
+end
