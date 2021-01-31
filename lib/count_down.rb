60f97debe17bfd9338435b2930f79a3dcb2d07b7
Mathew E Newton <elijah2895@hotmail.com>
2021-01-31 17:14:58 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -4,7 +4,6 @@ magic_exit_number = 10
 
 while count < 12 do
   break if count == magic_exit_number
-  puts "[10, 9, 8, 7, 6, 5, 4, 3, 2, 1]"
-  puts "Happy New Year!"
+  puts \n 10, \n 9, \n 8, \n 7, \n 6, \n 5, \n 4, \n 3, \n 2, \n 1
   count += 1
 end
